07b39f089de2dd3161d4e496ef8e3a68c38fe6eb
DaaseAllan <laurits.mumberg@gmail.com>
2019-03-21 10:24:30 +0100

    Upload af projekt

---
+++ LMAO/Assets/GameManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+    public static GameObject LocalPlayerInstance;
+
+    // Use this for initialization
+    void Start () {
+        PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LMAO/Assets/PhotonLobby.cs
@@ -0,0 +1,80 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks {
+
+    public static PhotonLobby lobby;
+
+    public GameObject connectButton;
+    public GameObject cancenButton;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    // Use this for initialization
+    void Start() {
+        PhotonNetwork.ConnectUsingSettings();
+        print("Prøver at connecte");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Player connected to master");
+        connectButton.SetActive(true);
+
+    }
+
+    public void JoinRandomGame()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        connectButton.SetActive(false);
+        cancenButton.SetActive(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        print("Failed to join random room, creating room");
+        CreateRoom();
+
+    }
+
+    void CreateRoom()
+    {
+        //Laver rummet
+        int randomRoomNumber = Random.Range(1, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
+
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        //Laver rummet igen. Denne fejl kan også ske, hvis at navnet allerede er taget
+        print("Room create failed. Trying again");
+        CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("Joined room with " + PhotonNetwork.CountOfPlayersInRooms + " players");
+        cancenButton.SetActive(false);
+
+    }
+
+    public void CanelJoin()
+    {
+        cancenButton.SetActive(false);
+        connectButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    } 
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
