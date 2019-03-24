8d8c1ee07b55cd9b6050f803b863e611cd40dd50
DaaseAllan <laurits.mumberg@gmail.com>
2019-03-24 14:21:32 +0100

    camera follow

---
+++ LMAO/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,65 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviourPunCallbacks
+{
+
+    public Transform target;
+    public bool followPlayer = true;
+    public float smoothTime = 0.3f;
+    public float xOffset;
+    public float yOffset;
+
+    private Vector3 velocity = Vector3.zero;
+    private GameObject ballToFollow;
+
+    private void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (followPlayer)
+        {
+            FollowPLayer();
+        }
+    }
+
+    private void FollowPLayer()
+    {
+        if (target == null)
+        {
+            GameObject[] AllCurrentBalls;
+            AllCurrentBalls = GameObject.FindGameObjectsWithTag("Player");
+
+            foreach (GameObject ball in AllCurrentBalls)
+            {
+                print("no");
+                if (ball.GetComponent<PhotonView>().IsMine)
+                {
+                    print("Nice");
+                    target = ball.transform;
+                }
+            }
+        }
+        else
+        {
+
+            Vector3 goalPos = target.position;
+
+            //Sikrer at den ikke flyver ned i banen
+            goalPos.z = transform.position.z;
+
+            //Tilføjer offset
+            goalPos.x += xOffset;
+            goalPos.y += yOffset;
+
+            transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+        }
+
+    }
+}
