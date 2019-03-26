305b6f05f2383818c11c6e61189c9a40ed8f01b0
Laurits <laurits.mumberg@gmail.com>
2019-03-26 19:49:03 +0100

    Sej roterende forhindring

---
+++ LMAO/Assets/Scripts/RotateWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWall : MonoBehaviour
+{
+
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Random.value < 0.5f)
+        {
+            rotateSpeed = -rotateSpeed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(new Vector3(0,0,1) * rotateSpeed * Time.deltaTime);
+    }
+}
