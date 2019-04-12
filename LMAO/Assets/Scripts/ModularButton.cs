6d961eaae1714f6eab6dd4aaa67d07c8fe87499d
DunkeyDunk <Malthe23@live.dk>
2019-04-12 10:52:55 +0200

    New prefab + Modularbutton

---
+++ LMAO/Assets/Scripts/ModularButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ModularButton : MonoBehaviour
+{
+    public bool delayActive = false;
+    public float delayTime;
+    private float startTime;
+    private Light light;
+
+    public UnityEvent OnPressed;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        if (delayActive)
+        {
+            if (Time.time - startTime > delayTime)
+            {
+                delayActive = false;
+                GetComponent<Animator>().Play("buttonUp");
+                light.enabled = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (delayActive)
+        {
+            return;
+        }
+
+        if (collision.transform.tag == "Player")
+        {
+            GetComponent<Animator>().Play("buttonDown");
+            OnPressed.Invoke();
+            delayActive = true; 
+            startTime = Time.time;
+            light.enabled = false;
+            print(light +" is "+ light.intensity.ToString());
+        }
+    }
+}
