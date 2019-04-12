675f7c953c8469673eff16253cd71c3af11ec936
DunkeyDunk <Malthe23@live.dk>
2019-04-12 16:07:31 +0200

    healthbar start (not done)

---
+++ LMAO/Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int health;
+    
+    //slider on canvas
+    public Slider healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthbar.value = health / maxHealth;
+        if (health < 0 )
+        {
+            //die here
+        }
+    }
+
+    void DealDamage(int dmg)
+    {
+        health -= dmg;
+    }
+}
