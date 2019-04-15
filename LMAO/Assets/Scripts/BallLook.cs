cbcd7ecbd5720d6f3f29ce8f365b7f0e52fcf69f
Jacob <jacobhlnhtxb16@gmail.com>
2019-04-15 10:44:53 +0200

    Trails virker (ihvertfald på menuen)

--- LMAO/Assets/Scripts/BallLook.cs
+++ LMAO/Assets/Scripts/BallLook.cs
@@ -8,17 +8,26 @@ public class BallLook : MonoBehaviourPunCallbacks
 {
 
     public enum BallColor {White, Red, Green, Blue}
+    public enum BallTrail {Fire, Water}
+
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
+
+    private TrailRenderer tr;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        tr = gameObject.GetComponentInChildren<TrailRenderer>();
         SetballColor();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SetballColor()
@@ -43,5 +52,23 @@ public class BallLook : MonoBehaviourPunCallbacks
                 break;
         }
 
+    }
+
+    public void SetTrailColor()
+    {
+        BallTrail thisBallsTrail = (BallTrail)photonView.Owner.CustomProperties["ballcolor"];
+        switch (thisBallsTrail)
+        {
+            case BallTrail.Fire:
+                tr.startColor = Color.yellow;
+                tr.endColor = Color.red;
+                gameObject.GetComponentInChildren<TrailRenderer>().colorGradient = tr.colorGradient;
+                break;
+            case BallTrail.Water:
+                tr.startColor = Color.blue;
+                tr.endColor = Color.white;
+                gameObject.GetComponentInChildren<TrailRenderer>().colorGradient = tr.colorGradient;
+                break;
+        }
     }
 }
--- LMAO/Assets/Scripts/SkinChange.cs
+++ LMAO/Assets/Scripts/SkinChange.cs
@@ -13,9 +13,18 @@ public class SkinChange : MonoBehaviourPunCallbacks
     private Hashtable playerInfoTable = new Hashtable();
     Animator animator;
 
+    public BallLook.BallTrail yourTrailColor;
+
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
+
+    private TrailRenderer tr;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        tr = GetComponentInChildren<TrailRenderer>();
     }
 
     public void SetBallColor()
@@ -40,8 +49,32 @@ public class SkinChange : MonoBehaviourPunCallbacks
                 yourBallColor = BallLook.BallColor.Blue;
                 prewiewBall.GetComponent<Renderer>().material.color = Color.blue;
                 break;
+            default:
+                break;
         }
         animator.SetInteger("ColorChange", 0);
+    }
+
+    public void SetTrailColor()
+    {
+        int trail = animator.GetInteger("TrailChange");
+        print(trail + " trail");
+        switch (trail)
+        {
+            case 1:
+                tr.startColor = Color.yellow;
+                tr.endColor = Color.red;
+                yourTrailColor = BallLook.BallTrail.Fire;
+                prewiewBall.GetComponentInChildren<TrailRenderer>().colorGradient = tr.colorGradient;
+                break;
+            case 2:
+                tr.startColor = Color.blue;
+                tr.endColor = Color.white;
+                yourTrailColor = BallLook.BallTrail.Water;
+                prewiewBall.GetComponentInChildren<TrailRenderer>().colorGradient = tr.colorGradient;
+                break;
+        }
+        animator.SetInteger("TrailChange", 0);
     }
 
 
@@ -51,9 +84,15 @@ public class SkinChange : MonoBehaviourPunCallbacks
         animator.SetInteger("ColorChange", nextColorInt);
     }
 
+    public void ChangeToNextTrail(int nextTrailInt)
+    {
+        animator.SetInteger("TrailChange", nextTrailInt);
+    }
+
     public void SubmitPlayerInfo()
     {
         playerInfoTable.Add("ballcolor", yourBallColor);
+        playerInfoTable.Add("ballTrail", yourTrailColor);
         PhotonNetwork.LocalPlayer.SetCustomProperties(playerInfoTable);
     }
 }
