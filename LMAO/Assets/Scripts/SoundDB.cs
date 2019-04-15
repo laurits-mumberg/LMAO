246db7371b11fccc567e413afe4c0cea58ce1a11
Steven <thedankpure@gmail.com>
2019-04-15 20:17:36 +0200

    arkitektur til lyd

---
+++ LMAO/Assets/Scripts/SoundDB.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDB : MonoBehaviour
+{
+    public AudioClip[] BGM;
+    public AudioClip[] soundFX;
+    public AudioClip[] menuSounds;
+}
---
+++ LMAO/Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+    public static SoundHandler soundHandler;
+    private AudioSource audioSource;
+
+    private static bool created = false;
+
+    private void Awake()
+    {
+        SoundHandlerInstanceCheck();
+    }
+
+    private void SoundHandlerInstanceCheck()
+    {
+        if (!created)
+        {
+            created = true;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+        soundHandler = this.GetComponent<SoundHandler>();
+    }
+
+    public void PlaySoundFX(AudioClip sound)
+    {
+        audioSource.PlayOneShot(sound);
+    }
+
+    public void PlayMusic(AudioClip sound)
+    {
+        audioSource.Play();
+    }
+}
